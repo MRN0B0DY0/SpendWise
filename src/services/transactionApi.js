0fa9351cf4b3b4b14123bc4bfa3840649f2d8d50
Omar <omarahmed026570@gmail.com>
2026-09-21 14:11:25 +0530

    deploy check

--- src/services/transactionApi.js
+++ src/services/transactionApi.js
@@ -249,7 +249,7 @@ export const getProfileStats = async () => {
 
 export const loginUser = async (email, password) => {
     const response = await fetch(
-        "http://localhost:5000/login",
+        "spendwise-backend-lime.vercel.app/login",
         {
             method: "POST",
             headers: {
@@ -274,7 +274,7 @@ export const loginUser = async (email, password) => {
 export const getCurrentUser = async () => {
     const token = localStorage.getItem("token");
     const response = await fetch(
-        "http://localhost:5000/me",
+        "spendwise-backend-lime.vercel.app/me",
         {
             headers: {
                 Authorization: `Bearer ${token}`
